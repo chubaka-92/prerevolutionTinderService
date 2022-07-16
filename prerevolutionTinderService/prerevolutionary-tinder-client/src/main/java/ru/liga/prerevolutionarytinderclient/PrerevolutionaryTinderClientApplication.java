9a81d24651b69fcbd8c157ad718bc2575c6428c2
chubaka-92 <chubaka_92@mail.ru>
2022-07-16 11:22:10 +0300

    multi project

---
+++ prerevolutionTinderService/prerevolutionary-tinder-client/src/main/java/ru/liga/prerevolutionarytinderclient/PrerevolutionaryTinderClientApplication.java
@@ -0,0 +1,13 @@
+package ru.liga.prerevolutionarytinderclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrerevolutionaryTinderClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrerevolutionaryTinderClientApplication.class, args);
+    }
+
+}
